c9add0502d1d13d0ab9935072233f0885e798129
VukSpalajkovic <vukspalajkovic@gmail.com>
2017-01-16 22:57:15 +0100

    16.1.17

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,35 @@
+#include "FBullCowGame.h"
+
+FBullCowGame::FBullCowGame()
+{
+	Reset();
+}
+
+void FBullCowGame::Reset()
+{
+	constexpr int MAX_TRIES = 8;
+	MyMaxTries = MAX_TRIES;
+	
+	MyCurrentTry = 1;
+	return;
+}
+
+int FBullCowGame::GetMaxTries() const
+{
+	return MyMaxTries;
+}
+
+int FBullCowGame::GetCurrentTry() const
+{
+	return MyCurrentTry;
+}
+
+bool FBullCowGame::IsGameWon() const
+{
+	return false;
+}
+
+bool FBullCowGame::CheckGuessValidity(std::string)
+{
+	return false;
+}
--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
-
-using namespace std;
+#include "FBullCowGame.h"
 
 void PrintIntro();
 void PlayGame();
-string GetGuess();
+std::string GetGuess();
 bool AskToPlayAgain();
 
+FBullCowGame BCGame; // instatiate a new game
 
-// Entry point for the application
+// entry point for the application
 int main()
 {
+	std::cout << BCGame.GetCurrentTry();
 	bool bPlayAgain = false;
 	do
 	{
@@ -20,45 +21,53 @@ int main()
 		bPlayAgain = AskToPlayAgain();
 	} while (bPlayAgain);
 
-	return 0; // Exit the application
+	return 0; // exit the application
 }
 
-// Introduce the game
+// introduce the game
 void PrintIntro()
 {
-	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows!\n\n" << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n" << endl;
+	constexpr int WORD_LENGTH = 9;
+	std::cout << "Welcome to Bulls and Cows!\n\n" << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n" << std::endl;
 	return;
 }
 
 void PlayGame()
 {
-	// Loop for the number of turns asking for guesses
-	constexpr int NUMBER_OF_TURNS = 5;
-	for (int i = 1; i <= NUMBER_OF_TURNS; i++)
+	BCGame.Reset();
+	int MaxTries = BCGame.GetMaxTries();
+	
+	// loop for the number of turns asking for guesses
+	// TODO change from FOR to WHILE loop once we are validating tries
+	for (int i = 1; i <= MaxTries; i++)
 	{
-		string Guess = GetGuess();
-		cout << "Your guess is: " << Guess << endl;
-		cout << endl;
-	}
+		std::string Guess = GetGuess(); //TODO make loop checking valid
 
+		// pubmit valid guess the game
+		// print number of bulls and cows
+
+		std::cout << "Your guess is: " << Guess << std::endl;
+		std::cout << std::endl;
+	}
+	// TODO summarize game
 }
 
-// Get a guess from the player and print the guess back to them
-string GetGuess()
+// get a guess from the player and print the guess back to them
+std::string GetGuess()
 {
-	// Get a guess from the player
-	cout << "Enter your guess: ";
-	string Guess = "";
-	getline(cin, Guess);
+	int CurrentTry = BCGame.GetCurrentTry();
+	// get a guess from the player
+	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+	std::string Guess = "";
+	std::getline(std::cin, Guess);
 	return Guess;
 }
 
 bool AskToPlayAgain()
 {
-	cout << "Do you want to play again? (y/n)";
-	string Response = "";
-	getline(cin, Response);
+	std::cout << "Do you want to play again? (y/n)";
+	std::string Response = "";
+	std::getline(std::cin, Response);
 	return (Response[0] == 'y') || (Response[0] == 'Y');;
-	cout << endl;
+	std::cout << std::endl;
 }
